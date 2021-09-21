22b2ccf3a0aa34b2f116cc140dccd236b8f3e630
Nikita Selyavin <nselyavin@inbox.ru>
2021-09-21 21:25:31 +0300

    Sort by array from get

--- src/index.php
+++ src/index.php
@@ -5,6 +5,31 @@
 </head>
 <body>
 <h1>Хочу пивка</h1>
+
+<?php
+    $array = $_GET["arr"];
+
+    foreach($array as $elem){
+        echo($elem );
+        echo(", ");
+    }
+
+    $len = count($array);
+    for($i = 0; $i < $len; $i++){
+        for($j = 1; $j < $len; $j++){
+            $k = $j - 1;
+            if($array[$j] < $array[$k]){
+                list($array[$j], $array[$k]) = array($array[$k], $array[$j]);
+            }
+        }
+    }
+    echo("<br> ");
+    foreach($array as $elem){
+        echo($elem );
+        echo(", ");
+    }
+?>
+
 <?php
     phpinfo();
 ?>
